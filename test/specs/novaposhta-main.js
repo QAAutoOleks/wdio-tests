6fa6046a8b62e43f39b267c64da76a1430bdf7dc
Oleksandr Tsupko <oleksandr.tsupko@gmail.com>
2024-02-29 12:01:18 +0200

    modified:   test/specs/novaposhta-main.js

--- test/specs/novaposhta-main.js
+++ test/specs/novaposhta-main.js
@@ -2,7 +2,7 @@ import { expect } from '@wdio/globals'
 
 
 describe('Nova Poshta main page', () => {
-    it('addValue to search field', async () => {
+    xit('main page getTitle', async () => {
         await browser.url('https://novaposhta.ua/')
         await browser.pause(1000)
         const title = await browser.getTitle()
@@ -10,5 +10,16 @@ describe('Nova Poshta main page', () => {
         
         await expect(browser).toHaveTitle('Термінова і експрес доставка: транспортно-логістичні послуги в Києві та по всій Україні - служба доставки №1 «Нова пошта»')
         
+    })
+    
+    it('addValue to search field', async () => {
+        await browser.url('https://novaposhta.ua/')
+        await browser.pause(1000)
+        
+        let searchField = await $('#cargo_number')
+        await searchField.addValue('String')
+        await browser.pause(1000)
+
+        await expect(searchField).toHaveValue('String')
     })
 })
